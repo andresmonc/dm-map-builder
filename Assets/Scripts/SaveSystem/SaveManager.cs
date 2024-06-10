ac888ac41ed747bce3c0dd2627d7d24779cc89c6
andresmonc <jaimeamonc@gmail.com>
2024-06-09 22:08:17 -0500

    SerializeSaveables into savedatacontainer

--- Assets/Scripts/SaveSystem/SaveManager.cs
+++ Assets/Scripts/SaveSystem/SaveManager.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 public class SaveManager : MonoBehaviour
 {
     private static SaveManager instance;
     private IEnumerable<ISaveable> saveables;
+    private static string path = Application.persistentDataPath + "/gamesave";
+    private static BinaryFormatter formatter = new BinaryFormatter();
+
 
     [Header("References")]
     [SerializeField] private float saveFrequency = 60f * 5f;
 
+    private void Start()
+    {
+        // Find and register saveables. Very expensive but should only happen on start.
+        RegisterSaveables();
+    }
 
     public static SaveManager Instance
     {
@@ -26,10 +36,35 @@ public class SaveManager : MonoBehaviour
         }
     }
 
-    private void registerSaveables()
+    private void RegisterSaveables()
     {
         saveables = FindObjectsOfType<MonoBehaviour>().OfType<ISaveable>();
-
     }
 
+    private void SerializeSaveables()
+    {
+        List<SaveData> saveData = new List<SaveData>();
+        foreach (ISaveable saveable in saveables)
+        {
+            saveData.Add(saveable.Save());
+        }
+        SaveDataContainer container = new SaveDataContainer { SaveData = saveData };
+        FileStream fileStream = new FileStream(path, FileMode.Create);
+        formatter.Serialize(fileStream, container);
+    }
+
+    private void DeserializeSaveables()
+    {
+        if (File.Exists(path))
+        {
+            using (FileStream fileStream = new FileStream(path, FileMode.Open))
+            {
+                SaveDataContainer container = (SaveDataContainer)formatter.Deserialize(fileStream);
+                foreach (SaveData saveData in container.SaveData)
+                {
+                    // Saveable.GetByID(xxxx).Load(saveData)?                    
+                }
+            }
+        }
+    }
 }
