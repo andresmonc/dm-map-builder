cc6a26e4c37c2d34f697970c970619336ed337b4
andresmonc <jaimeamonc@gmail.com>
2024-06-10 17:50:54 -0500

    make async saving

--- Assets/Scripts/SaveSystem/SaveManager.cs
+++ Assets/Scripts/SaveSystem/SaveManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class SaveManager : MonoBehaviour
@@ -14,12 +15,15 @@ public class SaveManager : MonoBehaviour
 
 
     [Header("References")]
-    [SerializeField] private float saveFrequency = 60f * 5f;
+    [SerializeField] private float saveFrequency = 60f * 5f; // Default to 5 minutes
 
     private void Start()
     {
         // Find and register saveables. Very expensive but should only happen on start.
         RegisterSaveables();
+
+        // Start the periodic saving
+        InvokeRepeating(nameof(ScheduleSerializeSaveables), saveFrequency, saveFrequency);
     }
 
     public static SaveManager Instance
@@ -41,7 +45,12 @@ public class SaveManager : MonoBehaviour
         saveables = FindObjectsOfType<MonoBehaviour>().OfType<ISaveable>();
     }
 
-    private void SerializeSaveables()
+    private async void ScheduleSerializeSaveables()
+    {
+        await SerializeSaveables();
+    }
+
+    private async Task SerializeSaveables()
     {
         List<SaveData> saveData = new List<SaveData>();
         foreach (ISaveable saveable in saveables)
@@ -49,8 +58,14 @@ public class SaveManager : MonoBehaviour
             saveData.Add(saveable.Save());
         }
         SaveDataContainer container = new SaveDataContainer { SaveData = saveData };
-        FileStream fileStream = new FileStream(path, FileMode.Create);
-        formatter.Serialize(fileStream, container);
+
+        await Task.Run(() =>
+        {
+            using (FileStream fileStream = new FileStream(path, FileMode.Create))
+            {
+                formatter.Serialize(fileStream, container);
+            }
+        });
     }
 
     private void DeserializeSaveables()
