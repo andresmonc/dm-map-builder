7332e9a6f65e8c8d61584610d4fd8469b29e4e0c
andresmonc <jaimeamonc@gmail.com>
2024-06-15 15:45:20 -0500

    new singleton

--- Assets/Scripts/LevelEditor/BuildingCreator.cs
+++ Assets/Scripts/LevelEditor/BuildingCreator.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class BuildingCreator : MonoBehaviour
+public class BuildingCreator : Singleton<BuildingCreator>
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerInput playerInput;
+
+    Vector2 mousePos;
+
+    protected override void Awake()
     {
-        
+        base.Awake();
+        playerInput = new PlayerInput();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+
     }
+
+    private void OnDisable()
+    {
+
+    }
+
 }
