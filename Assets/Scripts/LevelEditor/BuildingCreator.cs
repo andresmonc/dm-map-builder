5940f46c8ac531f8d242eaae9b964c827833a2c7
andresmonc <jaimeamonc@gmail.com>
2024-06-15 15:58:34 -0500

    controls changes

--- Assets/Scripts/LevelEditor/BuildingCreator.cs
+++ Assets/Scripts/LevelEditor/BuildingCreator.cs
@@ -1,28 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class BuildingCreator : Singleton<BuildingCreator>
 {
-    PlayerInput playerInput;
-
+    Controls controls;
     Vector2 mousePos;
 
     protected override void Awake()
     {
         base.Awake();
-        playerInput = new PlayerInput();
+        controls = new Controls();
     }
 
     private void OnEnable()
     {
+        controls.LevelEditorPlayer.MousePosition.performed += HandleMouseMove;
+        controls.LevelEditorPlayer.MouseLeftClick.performed += HandleMouseLeftClick;
+        controls.LevelEditorPlayer.MouseRightClick.performed += HandleMouseRightClick;
 
     }
 
     private void OnDisable()
     {
-
+        controls.LevelEditorPlayer.MousePosition.performed -= HandleMouseMove;
+        controls.LevelEditorPlayer.MouseLeftClick.performed -= HandleMouseLeftClick;
+        controls.LevelEditorPlayer.MouseRightClick.performed -= HandleMouseRightClick;
     }
+
+
+    private void HandleMouseRightClick(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleMouseLeftClick(InputAction.CallbackContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleMouseMove(InputAction.CallbackContext context)
+    {
+        mousePos = context.ReadValue<Vector2>();
+    }
+
+
 
 }
