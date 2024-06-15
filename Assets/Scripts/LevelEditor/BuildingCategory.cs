05b0de1c78430a3866a721d04a94033ca4cd57c0
andresmonc <jaimeamonc@gmail.com>
2024-06-15 18:57:14 -0500

    refactor

---
+++ Assets/Scripts/LevelEditor/BuildingCategory.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenuAttribute(fileName = "BuildingCategory", menuName = "Level Building/Create Category")]
+public class BuildingCategory : ScriptableObject
+{
+    [SerializeField] public PlaceType PlaceType { get; private set; }
+    [SerializeField] int sortingOrder = 0;
+    Tilemap tilemap;
+}
+
+public enum PlaceType
+{
+    None,
+    Single,
+    Line,
+    Rectangle
+}
--- Assets/Scripts/LevelEditor/BuildingObjectBase.cs
+++ Assets/Scripts/LevelEditor/BuildingObjectBase.cs
@@ -3,25 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-[CreateAssetMenu(fileName = "Buildable", menuName = "Building Objects/Create Buildable")]
+[CreateAssetMenu(fileName = "Buildable", menuName = "Level Building/Create Buildable")]
 public class BuildingObjectBase : ScriptableObject
 {
-    [field: SerializeField] public Category Category { get; private set; }
+    [field: SerializeField] public BuildingCategory Category { get; private set; }
     [field: SerializeField] public TileBase TileBase { get; private set; }
-    [field: SerializeField] public PlaceType PlaceType { get; private set; }
+    [SerializeField] private PlaceType placeType;
+
+    public PlaceType PlaceType
+    {
+        get
+        {
+            return placeType == PlaceType.None ? Category.PlaceType : placeType;
+        }
+    }
 
 
-}
-
-public enum Category
-{
-    Wall,
-    Floor
-}
-
-public enum PlaceType
-{
-    Single,
-    Line,
-    Rectangle
 }
