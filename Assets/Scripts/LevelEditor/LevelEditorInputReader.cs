f724fbfaff35cb2aebfd7731b2f9947e89ead626
andresmonc <jaimeamonc@gmail.com>
2024-06-15 16:58:51 -0500

    create input reader

--- Assets/Scripts/LevelEditor/LevelEditorInputReader.cs
+++ Assets/Scripts/LevelEditor/LevelEditorInputReader.cs
@@ -1,18 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using static Controls;
 
-public class LevelEditorInputReader : MonoBehaviour
+
+[CreateAssetMenu(fileName = "New Level Editor Input Reader", menuName = "Input/Input Reader")]
+public class LevelEditorInputReader : ScriptableObject, ILevelEditorPlayerActions
 {
-    // Start is called before the first frame update
-    void Start()
+    Controls controls;
+    public event Action<bool> LeftClickEvent;
+    public event Action<bool> RightClickEvent;
+    public Vector2 MousePosition { get; private set; }
+
+
+    private void OnEnable()
     {
-        
+        if (controls == null)
+        {
+            controls = new Controls();
+        }
+        controls.LevelEditorPlayer.Enable();
+        Debug.Log("Subscribing to changes!");
+        controls.LevelEditorPlayer.MousePosition.performed += OnMousePosition;
+        controls.LevelEditorPlayer.MouseLeftClick.performed += OnMouseLeftClick;
+        controls.LevelEditorPlayer.MouseRightClick.performed += OnMouseRightClick;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        Debug.Log("Unsubscribing to changes!");
+        controls.LevelEditorPlayer.MousePosition.performed -= OnMousePosition;
+        controls.LevelEditorPlayer.MouseLeftClick.performed -= OnMouseLeftClick;
+        controls.LevelEditorPlayer.MouseRightClick.performed -= OnMouseRightClick;
     }
+
+
+    public void OnMouseRightClick(InputAction.CallbackContext context)
+    {
+        SendClickEvent(context, RightClickEvent);
+    }
+
+    public void OnMouseLeftClick(InputAction.CallbackContext context)
+    {
+        SendClickEvent(context, LeftClickEvent);
+    }
+
+    public void OnMousePosition(InputAction.CallbackContext context)
+    {
+        MousePosition = context.ReadValue<Vector2>();
+    }
+
+
+    private void SendClickEvent(InputAction.CallbackContext context, Action<bool> clickAction)
+    {
+        if (context.performed)
+        {
+            clickAction?.Invoke(true);
+        }
+        else if (context.canceled)
+        {
+            clickAction?.Invoke(false);
+        }
+    }
+
 }
