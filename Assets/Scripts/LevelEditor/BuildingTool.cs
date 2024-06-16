cba7121b699a8a91f5f7e1b73561d0bcfcc786ef
andresmonc <jaimeamonc@gmail.com>
2024-06-16 17:25:46 -0500

    updates

---
+++ Assets/Scripts/LevelEditor/BuildingTool.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Buildable", menuName = "Level Building/Create Tool")]
+public class BuildingTool : BuildingObjectBase
+{
+
+}
